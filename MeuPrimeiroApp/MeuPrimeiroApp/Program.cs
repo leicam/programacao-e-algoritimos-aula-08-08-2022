c89aeeeee0cca4f856e0b5ebfdb8e869e19539ae
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-08-08 20:31:47 -0300

    Implementação meu primeiro programa

---
+++ MeuPrimeiroApp/MeuPrimeiroApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace MeuPrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva seu nome:");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Aguarde...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Olá: " + nome);
+        }
+    }
+}
